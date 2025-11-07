7ed897bb143c3f216ad8c0cca8382378cdd10374
CalPogi <titusplaza1202@gmail.com>
2025-11-07 11:11:58 +0800

    Add Kernel, middleware and cors config

---
+++ app/Http/Middleware/LogOptionsRequests.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class LogOptionsRequests
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->isMethod('OPTIONS')) {
+            // You can log headers or just return an empty response
+            return response('', 204)
+                ->header('Access-Control-Allow-Origin', $request->header('Origin'))
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'))
+                ->header('Access-Control-Allow-Credentials', 'true');
+        }
+        return $next($request);
+    }
+}
