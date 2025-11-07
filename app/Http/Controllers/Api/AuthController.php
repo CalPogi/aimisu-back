b7e2598c0e6d77d153d58e2cd030a17a22ae962f
CalPogi <titusplaza1202@gmail.com>
2025-11-07 12:15:20 +0800

    Add Kernel, middleware and cors config

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'email' => 'required|email|exists:users',
+            'email' => 'required|email',
             'password' => 'required',
         ]);
 
