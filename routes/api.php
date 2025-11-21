4d0d69a4e2da3ec961f7ee5a11c77656350a6048
CalPogi <titusplaza1202@gmail.com>
2025-11-21 21:20:44 +0800

    fixed fetch events

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,16 @@ use App\Http\Controllers\Api\EventController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::get('/events', [EventController::class, 'index']);
+Route::get('/events/{event}', [EventController::class, 'show']);
+
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::apiResource('/users', UserController::class);
-    Route::apiResource('/events', EventController::class);
+
+    Route::post('/events', [EventController::class, 'store']);
+    Route::put('/events/{event}', [EventController::class, 'update']);
+    Route::delete('/events/{event}', [EventController::class, 'destroy']);
 
 
     Route::post('/logout', [AuthController::class, 'logout']);
