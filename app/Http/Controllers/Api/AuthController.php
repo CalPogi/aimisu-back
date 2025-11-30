fe13f3a2d3371e48b123569786d6a34f616ff21c
CalPogi <titusplaza1202@gmail.com>
2025-11-30 19:34:49 +0800

    Commit again ya

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
